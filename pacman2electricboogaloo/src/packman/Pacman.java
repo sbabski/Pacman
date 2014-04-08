c289f0ff1101be0dfaa0ce0f28f79f139205c0a9
sbabski <sbabski@zimbra.ccs.neu.edu>
2014-04-08 15:12:52 -0400

    Update Pacman.java

--- pacman2electricboogaloo/src/packman/Pacman.java
+++ pacman2electricboogaloo/src/packman/Pacman.java
@@ -1,16 +1,21 @@
 package packman;
 
 public class Pacman extends Character {
-	
+	Integer score;
 	Integer spriteNumber;
 	boolean spriteIncreasing;
 	
-	Pacman(boolean v, boolean p, Integer c, Integer pa, Integer sn, boolean si) {
+	Pacman(boolean v, boolean p, Integer c, Integer pa, Integer sn, boolean si, Integer sc) {
 		super(v, p, c, pa);
 		this.spriteNumber = sn;
 		this.spriteIncreasing = si;
+		this.score = sc;
 	}
-
-	
-	
+	Pacman(Pacman p) {
+		super(p.verticalDirection,p.positiveDirection,
+				p.columnOrRow,p.positionOnOtherAxis);
+		this.spriteNumber = p.spriteNumber;
+		this.spriteIncreasing = p.spriteIncreasing;
+		this.score = p.score;
+	}
 }
